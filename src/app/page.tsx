20bc5639a4fdc2e7ddfe6e9ed1754a9da1fe7dd7
LIU CHIAWEI <doublecheap@gmail.com>
2025-05-08 13:29:48 +0900

    style: adjust top page title style

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -50,7 +50,10 @@ const featureList: {
 export default function Home() {
   return (
     <div className="flex flex-col items-center justify-center h-screen w-screen">
-      <h1 className="text-4xl font-bold mb-4">Hello AI</h1>
+      <div className="flex flex-col items-center justify-center gap-2 mb-6 tracking-wider">
+        <h1 className="text-4xl md:text-6xl font-bold">Hello, AI!</h1>
+        <h2 className="text-sm md:text-md font-normal text-zinc-500">Playground for Vercel AI SDK</h2>
+      </div>
       <div className="grid grid-cols-2 md:grid-cols-4 items-center justify-center gap-2">
         {featureList.map((feature) => (
           <Link href={feature.href} key={feature.name} title={feature.status} className={`border p-2 text-center hover:bg-zinc-700 ${feature.status === "done" ? "" : "bg-zinc-800"}`}>
